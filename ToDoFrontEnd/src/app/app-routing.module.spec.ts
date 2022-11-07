323b9da7c9f8eee39ae310a1469234010cc83dac
Siqi Song <ssong6@slb.com>
2022-11-07 14:47:26 +0800

    feat: add route for different components

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { routes } from './app-routing.module';
 
 describe('Router', () => {
   let location: Location;
@@ -9,7 +10,7 @@ describe('Router', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes(routes)],
       declarations: []
     });
 
@@ -19,7 +20,53 @@ describe('Router', () => {
     router.initialNavigation();
   });
 
-  it('test navigate', fakeAsync(() =>{
+  it('should go to todos when navigate ""', fakeAsync(() => {
+    // given
+    // when
+    router.navigate([''])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos')
+
+  }));
+
+  it('should go to todos when navigate "todos"', fakeAsync(() => {
+    // given
+    // when
+    router.navigate(['todos'])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos')
+
+  }));
+
+  it('should go to create when navigate "todos/create"', fakeAsync(() => {
+    // given
+    // when
+    router.navigate(['todos/create'])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/create')
+
+  }));
+
+  it('should go to update when navigate "todos/edit/1"', fakeAsync(() => {
+    // given
+    // when
+    router.navigate(['todos/edit',1])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/edit/1')
+
+  }));
+
+  it('should go to show detail when navigate "todos/2"', fakeAsync(() => {
+    // given
+    // when
+    router.navigate(['todos',2])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/2')
 
   }));
 });
