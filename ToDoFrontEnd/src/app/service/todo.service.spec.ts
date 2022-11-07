079c399bd9d184e4d168047de206658fc2c11e51
Jie Li <JLi147@slb.com>
2022-11-07 16:50:40 +0800

    test: add create todoItem test

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { ToDoItem } from './../model/ToDoItem';
 import { TestBed } from '@angular/core/testing';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
@@ -6,8 +8,10 @@ describe('TodoService', () => {
 
   let service: TodoService;
   let todoStoreService: TodoStoreService;
+  let httpClient: any;
 
   beforeEach(() => {
+    httpClient = jasmine.createSpyObj('HttpClient', ['post']);
     todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
@@ -15,5 +19,14 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create todoItem via mockHttp post', () => {
+    // given
+    const toDoItem = new ToDoItem(9, 'title', 'description', true);
+    // when
+    service.create(toDoItem);
+    // then
+    expect(httpClient.post).toHaveBeenCalledWith('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', toDoItem);
   });
 });
